6be7cd5e63cf8d1f05b6d8de71e33c7c976c595b
Christopher O'Brien <cobrien@redhat.com>
2022-02-25 13:12:56 -0500

    fix license, use package resource templates

--- charon/pkgs/indexing.py
+++ charon/pkgs/indexing.py
@@ -1,47 +1,36 @@
-"""
-Copyright (C) 2022 Red Hat, Inc. (https://github.com/Commonjava/charon)
+# Copyright (C) 2022 Red Hat, Inc. (https://github.com/Commonjava/charon)
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-         http://www.apache.org/licenses/LICENSE-2.0
+#          http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-from charon.config import get_template
-from charon.storage import S3Client
-from charon.constants import (INDEX_HTML_TEMPLATE, NPM_INDEX_HTML_TEMPLATE,
-                              PACKAGE_TYPE_MAVEN, PACKAGE_TYPE_NPM, PROD_INFO_SUFFIX)
-from jinja2 import Template
-import os
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import logging
+import os
+
+from jinja2 import Template
 from typing import List, Set
 
-from charon.utils.strings import remove_prefix
+from ..config import get_template
+from ..constants import (
+    PACKAGE_TYPE_MAVEN, PACKAGE_TYPE_NPM, PROD_INFO_SUFFIX, )
+from ..storage import S3Client
+from ..utils import remove_prefix
+
 
 logger = logging.getLogger(__name__)
 
 
-def __get_index_template(package_type: str) -> str:
-    """Gets the jinja2 template file content for index generation"""
-    try:
-        return get_template("index.html.j2")
-    except FileNotFoundError:
-        logger.info("index template file not defined,"
-                    " will use default template.")
-        if package_type == PACKAGE_TYPE_MAVEN:
-            return INDEX_HTML_TEMPLATE
-        elif package_type == PACKAGE_TYPE_NPM:
-            return NPM_INDEX_HTML_TEMPLATE
-
-
-MAVEN_INDEX_TEMPLATE = __get_index_template(PACKAGE_TYPE_MAVEN)
-NPM_INDEX_TEMPLATE = __get_index_template(PACKAGE_TYPE_NPM)
+MAVEN_INDEX_TEMPLATE = get_template("maven-index.html.j2")
+NPM_INDEX_TEMPLATE = get_template("npm-index.html.j2")
 
 
 class IndexedHTML(object):
