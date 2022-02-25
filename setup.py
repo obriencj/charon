c07c587036ee8afee57eaca7318392cba091751b
Christopher O'Brien <cobrien@redhat.com>
2022-02-25 10:17:22 -0500

    progress

--- setup.py
+++ setup.py
@@ -16,55 +16,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import re
-
 from setuptools import setup, find_packages
-
-version = "1.1.0"
-
-# f = open('README.md')
-# long_description = f.read().strip()
-# long_description = long_description.split('split here', 1)[1]
-# f.close()
-long_description = """
-This charon is a tool to synchronize several types of
-artifacts repository data to RedHat Ronda service (maven.repository.redhat.com).
-These repositories including types of maven, npm or some others like python
-in future. And Ronda service will be hosted in AWS S3.
-"""
-
-
-def _get_requirements(path):
-    try:
-        with open(path, encoding="utf-8") as f:
-            packages = f.read().splitlines()
-    except (IOError, OSError) as ex:
-        raise RuntimeError(f"Can't open file with requirements: {ex}") from ex
-    packages = (p.strip() for p in packages if not re.match(r'^\s*#', p))
-    packages = list(filter(None, packages))
-    return packages
 
 
 setup(
-    zip_safe=True,
-    name="charon",
-    version=version,
-    long_description=long_description,
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Utilities",
+    packages = [
+        'charon',
+        'charon.cmd',
+        'charon.pkgs',
+        'charon.utils',
     ],
-    keywords="charon mrrc maven npm build java",
-    author="RedHat EXD SPMM",
-    license="APLv2",
-    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
-    install_requires=_get_requirements('requirements.txt'),
-    test_suite="tests",
-    entry_points={
+
+    install_requires = [
+        'boto3',
+        'click',
+        'requests',
+    ],
+
+    entry_points = {
         "console_scripts": ["charon = charon:cli"],
     },
 )
+
+
+# The end.
