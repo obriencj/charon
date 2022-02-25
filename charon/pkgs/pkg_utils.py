1aa0268e19e21677df82731291b8a83ea37d7636
Christopher O'Brien <cobrien@redhat.com>
2022-02-25 13:13:24 -0500

    flake8

--- charon/pkgs/pkg_utils.py
+++ charon/pkgs/pkg_utils.py
@@ -7,46 +7,64 @@ logger = logging.getLogger(__name__)
 
 def is_metadata(file: str) -> bool:
     return is_mvn_metadata(file) or \
-           is_npm_metadata(file) or \
-           file.endswith("index.html")
+        is_npm_metadata(file) or \
+        file.endswith("index.html")
 
 
 def is_mvn_metadata(file: str) -> bool:
     return "maven-metadata.xml" in file or \
-           "archetype-catalog.xml" in file
+        "archetype-catalog.xml" in file
 
 
 def is_npm_metadata(file: str) -> bool:
     return "package.json" in file
 
 
-def upload_post_process(failed_files: List[str], failed_metas: List[str], product_key):
+def upload_post_process(
+        failed_files: List[str],
+        failed_metas: List[str], product_key):
+
     __post_process(failed_files, failed_metas, product_key, "uploaded to")
 
 
-def rollback_post_process(failed_files: List[str], failed_metas: List[str], product_key):
+def rollback_post_process(
+        failed_files: List[str],
+        failed_metas: List[str], product_key):
+
     __post_process(failed_files, failed_metas, product_key, "rolled back from")
 
 
 def __post_process(
-    failed_files: List[str],
-    failed_metas: List[str],
-    product_key: str,
-    operation: str
-):
+        failed_files: List[str],
+        failed_metas: List[str],
+        product_key: str,
+        operation: str):
+
     if len(failed_files) == 0 and len(failed_metas) == 0:
         logger.info("Product release is successfully %s "
                     " %s Ronda service.", operation, product_key)
+
     else:
         total = len(failed_files) + len(failed_metas)
-        logger.error("%d file(s) occur errors/warnings, please see errors.log for details.", total)
+
+        logger.error("%d file(s) occur errors/warnings,"
+                     " please see errors.log for details.", total)
+
         logger.error("Product release %s is %s Ronda "
                      "service, but has some failures as below:",
                      product_key, operation)
+
         if len(failed_files) > 0:
             logger.error("Failed files: \n%s",
                          failed_files)
+
         if len(failed_metas) > 0:
             logger.error("Failed metadata files: \n%s",
                          failed_metas)
+
+        # wtf?? TODO raise an exception that's caught somewhere
+        # sensible
         sys.exit(1)
+
+
+# The end.
