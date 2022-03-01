953422fb8900dcbd7242e3929f7dd7818416f9df
Christopher O'Brien <cobrien@redhat.com>
2022-03-01 09:21:40 -0500

    fix license, fix imports

--- charon/utils/logs.py
+++ charon/utils/logs.py
@@ -1,30 +1,32 @@
-"""
-Copyright (C) 2022 Red Hat, Inc. (https://github.com/Commonjava/charon)
+# Copyright (C) 2022 Red Hat, Inc. (https://github.com/Commonjava/charon)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
 
-         http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-import logging
 import sys
+import logging
 import os
+
 from locale import nl_langinfo, CODESET
 
-from charon.constants import CHARON_LOGGING_FMT, DEFAULT_ERRORS_LOG
+from ..constants import CHARON_LOGGING_FMT, DEFAULT_ERRORS_LOG
 
 
-class EncodedStream(object):
+class EncodedStream():
     # The point of this class is to force python to encode UTF-8
     # over stderr.  Normal techniques were not working, so we dup
     # the file handler and force it UTF-8.  :-(
+
     def __init__(self, fileno, encoding):
         self.binarystream = os.fdopen(os.dup(fileno), 'wb')
         self.encoding = encoding
